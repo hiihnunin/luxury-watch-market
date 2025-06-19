f0b96034c73404300cdd764115aff19c2a71766c
hiihnunin <a57051579@gmail.com>
2025-06-19 13:09:09 +0900

    Create  3. 장바구니 cart.html & cart.js
    ㅇ

---
+++ 3. 장바구니 cart.html & cart.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>장바구니</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h2>장바구니</h2>
+  <div id="cart-items"></div>
+  <button onclick="checkout()">결제하기</button>
+
+  <script src="cart.js"></script>
+</body>
+</html>
